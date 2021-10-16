6157429a906d36bbe4bc190245eca196e16b7dce
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-10-16 23:15:28 +0200

    doint it

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdressBook2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options)
+        {
+        }
+
+    }
+}
