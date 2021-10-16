642f28be8ea2798501ea9febefc349d3139acc19
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-10-16 19:22:01 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdressBook2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
