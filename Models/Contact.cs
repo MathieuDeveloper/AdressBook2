dd3df921f01c7be5730863c6576af4b6d6018148
MathieuDeveloper <topfermathieu@yahoo.fr>
2021-10-17 23:58:14 +0200

    still doing

---
+++ Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdressBook2.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public IFormFile ImageFile { get; set; }
+        public byte[] ImageData { get; set; }
+        public string ImageType { get; set; }
+
+    }
+}
